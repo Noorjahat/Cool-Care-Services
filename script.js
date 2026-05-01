cdff775a7f2d8a1640542e98bb1bf84311535c71
Noorjahat <galvestongamer21@gmail.com>
2026-05-01 22:26:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function callNow(){
+window.location.href="tel:8982767251";
+}
+
+function whatsapp(){
+window.open("https://wa.me/918982767251");
+}
+
+function direction(){
+window.open("https://maps.google.com/?q=Maihar");
+}
+
+function sendWhatsApp(){
+let name=document.getElementById("name").value;
+let phone=document.getElementById("phone").value;
+let address=document.getElementById("address").value;
+
+if(!name || !phone){
+alert("Fill Name & Phone");
+return;
+}
+
+let msg=`Name: ${name}%0APhone: ${phone}%0AAddress: ${address}`;
+window.open(`https://wa.me/918982767251?text=${msg}`);
+}
